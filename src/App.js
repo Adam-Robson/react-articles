46e9ebb5fe5b74926057447c4b6a638492806475
Adam Robson <adamray312@gmail.com>
2022-12-16 20:12:36 -0800

    setup components Home Article(s) and About and Not Found

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Articles from './components/Articles/Articles';
+import Article from './components/Article/Article';
+import About from './components/About/About';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This Darn Ghost</h1>
+      <div id="page-body">Welcome to this darn ghost!
+        <Home />
+        <About />
+        <Articles />
+        <Article />
+      </div>
     </div>
   );
 }
